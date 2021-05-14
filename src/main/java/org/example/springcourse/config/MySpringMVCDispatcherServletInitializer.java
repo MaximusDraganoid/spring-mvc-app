4573fca73720b2d6f01206bca6fc45a8ebbd057b
MaximusDraganoid <maslowmaxim@gmail.com>
2021-05-14 12:37:46 +0300

    configure spring mvc via java code

---
+++ src/main/java/org/example/springcourse/config/MySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package org.example.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    //здесь находится конфигурационный класс нашего спринг-приложения
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
